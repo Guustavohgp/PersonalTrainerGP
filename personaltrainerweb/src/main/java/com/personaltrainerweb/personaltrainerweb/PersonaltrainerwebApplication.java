ccd46dff01a4d7434c18b11c78b10ba14a9ca9b3
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-09-26 14:51:03 -0300

    create estrutura

---
+++ personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/PersonaltrainerwebApplication.java
@@ -0,0 +1,13 @@
+package com.personaltrainerweb.personaltrainerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaltrainerwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaltrainerwebApplication.class, args);
+	}
+
+}
