aedd76b45f9025f298701976c1fb2557af2e02a5
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-09-26 15:44:02 -0300

    att ajustando saida

--- personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/controller/UsuarioController.java
+++ personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/controller/UsuarioController.java
@@ -1,19 +1,19 @@
 package com.personaltrainerweb.personaltrainerweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class UsuarioController {
-    @GetMapping("/")
+
+    @GetMapping("/index")
     public String index() {
         return "index"; // O Spring Boot vai buscar o arquivo index.html em /src/main/resources/templates
     }
 
-    // Mapeia a página de resultado (result.html)
     @GetMapping("/result")
     public String result() {
         return "result"; // O Spring Boot vai buscar o arquivo result.html em /src/main/resources/templates
     }
 }
-
