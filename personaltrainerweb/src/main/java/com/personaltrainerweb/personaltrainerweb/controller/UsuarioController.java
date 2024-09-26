b47873274b7c7f9ae30fa9624c51cbad729388bc
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-09-26 15:08:24 -0300

    add class reat controller

--- personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/controller/UsuarioController.java
+++ personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/controller/UsuarioController.java
@@ -1,7 +1,9 @@
 package com.personaltrainerweb.personaltrainerweb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UsuarioController {
     @GetMapping("/usuarios")
     public String texto () {
