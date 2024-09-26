13a4e703256f80ed1893605840c6665d7edf8d3c
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-09-26 14:58:14 -0300

    att correção

--- personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/repository/UsuarioRepository.java
+++ personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/repository/UsuarioRepository.java
@@ -3,6 +3,8 @@ package com.personaltrainerweb.personaltrainerweb.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.personaltrainerweb.personaltrainerweb.model.Usuario;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
