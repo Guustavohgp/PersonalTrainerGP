ca984cf762183dd6e71be5db4457ba535cdce89a
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-09-26 14:57:54 -0300

    create usuario

---
+++ personaltrainerweb/src/main/java/com/personaltrainerweb/personaltrainerweb/model/Usuario.java
@@ -0,0 +1,68 @@
+package com.personaltrainerweb.personaltrainerweb.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private int idade;
+    private String biotipo;
+    private String treino;
+    private String experiencia;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getBiotipo() {
+        return biotipo;
+    }
+
+    public void setBiotipo(String biotipo) {
+        this.biotipo = biotipo;
+    }
+
+    public String getTreino() {
+        return treino;
+    }
+
+    public void setTreino(String treino) {
+        this.treino = treino;
+    }
+
+    public String getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
+
+}
