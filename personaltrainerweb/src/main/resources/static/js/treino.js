c69dee9ee55c313b0f3452a905578c5f0e2a59d9
Guuh <gustavo.henriquegpires@gmail.com>
2024-10-24 17:20:11 -0300

    att ajuste saida de treino

--- personaltrainerweb/src/main/resources/static/js/treino.js
+++ personaltrainerweb/src/main/resources/static/js/treino.js
@@ -4,38 +4,38 @@ function gerarRotinaTreino(params) {
     // Planos de treino Funcional
     const treinoFuncional = {
         iniciante: {
-            1: ["Dia 1: Corrida 10min, Flexões 3x10", "Pular Corda 5min, Abdominais 3x20", "Agachamentos 3x15", "Burpees 3x10", "Corrida 10min"],
-            3: ["Dia 1: Corrida 10min, Pular Corda 5min", "Dia 2: Agachamentos 3x15, Flexões 3x10", "Dia 3: Abdominais 3x20, Burpees 3x10"],
-            5: ["Dia 1: Corrida 10min, Flexões 3x10", "Dia 2: Pular Corda 5min, Abdominais 3x20", "Dia 3: Agachamentos 3x15", "Dia 4: Burpees 3x10", "Dia 5: Corrida 10min"]
+            1: ["Dia 1: Esteira ou Bike 15min, Flexões 3x10", "Pular Corda 5min, Abdominais 3x20", "Agachamentos 3x15", "Burpees 3x10", "Esteira ou Bike 15min"],
+            3: ["Dia 1: Esteira ou Bike 15min, Pular Corda 5min", "Dia 2: Agachamentos 3x15, Flexões 3x10", "Dia 3: Abdominais 3x20, Burpees 3x10"],
+            5: ["Dia 1: Esteira ou Bike 15min, Flexões 3x10", "Dia 2: Pular Corda 5min, Abdominais 3x20", "Dia 3: Agachamentos 3x15", "Dia 4: Burpees 3x10, Pular Corda 5min", "Dia 5: Esteira ou Bike 15min"]
         },
         intermediário: {
-            1: ["Dia 1: Corrida 20min, Flexões 4x15", "Pular Corda 10min, Abdominais 4x25", "Agachamentos 4x20", "Burpees 4x15", "Corrida 20min"],
-            3: ["Dia 1: Corrida 20min, Pular Corda 10min", "Dia 2: Agachamentos 4x20, Flexões 4x15", "Dia 3: Abdominais 4x25, Burpees 4x15"],
-            5: ["Dia 1: Corrida 20min, Flexões 4x15", "Dia 2: Pular Corda 10min, Abdominais 4x25", "Dia 3: Agachamentos 4x20", "Dia 4: Burpees 4x15", "Dia 5: Corrida 20min"]
+            1: ["Dia 1: Esteira ou Bike 20min, Flexões 4x15", "Pular Corda 10min, Abdominais 4x25", "Agachamentos 4x20", "Burpees 4x15", "Esteira ou Bike 20min"],
+            3: ["Dia 1: Esteira ou Bike 20min, Pular Corda 10min", "Dia 2: Agachamentos 4x20, Flexões 4x15", "Dia 3: Abdominais 4x25, Burpees 4x15"],
+            5: ["Dia 1: Esteira ou Bike 20min, Flexões 4x15", "Dia 2: Pular Corda 10min, Abdominais 4x25", "Dia 3: Agachamentos 4x20", "Dia 4: Burpees 4x15", "Dia 5: Esteira ou Bike 20min"]
         },
         avançado: {
-            1: ["Dia 1: Corrida 30min, Flexões 5x20", "Pular Corda 15min, Abdominais 5x30", "Agachamentos 5x20", "Burpees 5x20", "Corrida 30min"],
-            3: ["Dia 1: Corrida 30min, Pular Corda 15min", "Dia 2: Agachamentos 5x20, Flexões 5x20", "Dia 3: Abdominais 5x30, Burpees 5x20"],
-            5: ["Dia 1: Corrida 30min, Flexões 5x20", "Dia 2: Pular Corda 15min, Abdominais 5x30", "Dia 3: Agachamentos 5x20", "Dia 4: Burpees 5x20", "Dia 5: Corrida 30min"]
+            1: ["Dia 1: Esteira ou Bike 30min, Flexões 5x20", "Pular Corda 15min, Abdominais 5x30", "Agachamentos 5x20", "Burpees 5x20", "Esteira ou Bike30min"],
+            3: ["Dia 1: Esteira ou Bike 30min, Pular Corda 15min", "Dia 2: Agachamentos 5x20, Flexões 5x20", "Dia 3: Abdominais 5x30, Burpees 5x20"],
+            5: ["Dia 1: Esteira ou Bike 30min, Flexões 5x20", "Dia 2: Pular Corda 15min, Abdominais 5x30", "Dia 3: Agachamentos 5x20", "Dia 4: Burpees 5x20", "Dia 5: Esteira ou Bike 30min"]
         }
     };
 
     // Planos de treino Cardio
     const treinoCardio = {
         iniciante: {
-            1: ["Dia 1: Corrida 10min, Pular Corda 10min"],
-            3: ["Dia 1: Corrida 10min", "Dia 2: Pular Corda 10min", "Dia 3: Corrida 10min"],
-            5: ["Dia 1: Corrida 10min", "Dia 2: Pular Corda 10min", "Dia 3: Corrida 10min", "Dia 4: Pular Corda 10min", "Dia 5: Corrida 10min"]
+            1: ["Dia 1: Esteira ou Bike 15min, Pular Corda 10min"],
+            3: ["Dia 1: Esteira ou Bike 15min", "Dia 2: Pular Corda 10min", "Dia 3: Esteira ou Bike 15min"],
+            5: ["Dia 1: Esteira ou Bike 15min", "Dia 2: Pular Corda 10min", "Dia 3: Esteira ou Bike 15min", "Dia 4: Pular Corda 10min", "Dia 5: Esteira ou Bike 15min, Pular Corda 10min "]
         },
         intermediário: {
-            1: ["Dia 1: Corrida 20min, Pular Corda 15min"],
-            3: ["Dia 1: Corrida 20min", "Dia 2: Pular Corda 15min", "Dia 3: Corrida 20min"],
-            5: ["Dia 1: Corrida 20min", "Dia 2: Pular Corda 15min", "Dia 3: Corrida 20min", "Dia 4: Pular Corda 15min", "Dia 5: Corrida 20min"]
+            1: ["Dia 1: Esteira 20min, Pular Corda 15min"],
+            3: ["Dia 1: Esteira 20min", "Dia 2: Pular Corda 15min", "Dia 3: Esteira ou Bike 20min"],
+            5: ["Dia 1: Esteira 20min", "Dia 2: Pular Corda 15min", "Dia 3: Esteira ou Bike 20min", "Dia 4: Pular Corda 15min", "Dia 5: Esteira ou Bike 20min"]
         },
         avançado: {
-            1: ["Dia 1: Corrida 30min, Pular Corda 20min"],
-            3: ["Dia 1: Corrida 30min", "Dia 2: Pular Corda 20min", "Dia 3: Corrida 30min"],
-            5: ["Dia 1: Corrida 30min", "Dia 2: Pular Corda 20min", "Dia 3: Corrida 30min", "Dia 4: Pular Corda 20min", "Dia 5: Corrida 30min"]
+            1: ["Dia 1: Esteira ou Bike 30min, Pular Corda 20min"],
+            3: ["Dia 1: Esteira ou Bike 30min", "Dia 2: Pular Corda 20min", "Dia 3: Esteira ou Bike 30min"],
+            5: ["Dia 1: Esteira ou Bike 30min", "Dia 2: Pular Corda 20min", "Dia 3: Esteira ou Bike 30min", "Dia 4: Pular Corda 20min", "Dia 5: Esteira ou Bike 30min"]
         }
     };
 
@@ -51,7 +51,7 @@ function gerarRotinaTreino(params) {
                 "Pernas: Agachamento 3x15",
                 "Abdômen: Abdominais 3x20",
                 "Ombros: Elevação lateral 3x12",
-                "Cardio: Corrida 10min"
+                "Cardio: Esteira ou Bike 15min"
             ],
             3: [
                 "Dia 1: Peito e Tríceps",
@@ -64,12 +64,12 @@ function gerarRotinaTreino(params) {
                 "Dia 3: Pernas, Abdômen e Cardio",
                 "Pernas: Agachamento 3x15, Leg press 3x12",
                 "Abdômen: Abdominais 3x20",
-                "Cardio: Corrida 10min"
+                "Cardio: Esteira ou Bike 15min"
             ],
             5: [
                 "Dia 1: Peito e Cardio",
                 "Peito: Supino reto 3x12, Flexões 3x15",
-                "Cardio: Corrida 10min",
+                "Cardio: Esteira ou Bike 15min",
                 "Dia 2: Costas",
                 "Costas: Remada curvada 4x12, Puxada na polia 3x12",
                 "Dia 3: Pernas",
@@ -94,7 +94,7 @@ function gerarRotinaTreino(params) {
                 "Pernas: Agachamento 4x12",
                 "Abdômen: Abdominais 4x25",
                 "Ombros: Elevação lateral 4x10",
-                "Cardio: Corrida 20min"
+                "Cardio: Esteira ou Bike 20min"
             ],
             3: [
                 "Dia 1: Peito e Tríceps",
@@ -107,12 +107,12 @@ function gerarRotinaTreino(params) {
                 "Dia 3: Pernas, Abdômen e Cardio",
                 "Pernas: Agachamento 4x12, Leg press 4x12",
                 "Abdômen: Abdominais 4x25",
-                "Cardio: Corrida 20min"
+                "Cardio: Esteira ou Bike 20min"
             ],
             5: [
                 "Dia 1: Peito e Cardio",
                 "Peito: Supino reto 4x10, Flexões 4x12",
-                "Cardio: Corrida 20min",
+                "Cardio: Esteira ou Bike 20min",
                 "Dia 2: Costas",
                 "Costas: Remada curvada 4x12, Puxada na polia 4x10",
                 "Dia 3: Pernas",
@@ -135,7 +135,7 @@ function gerarRotinaTreino(params) {
                 "Pernas: Agachamento 5x12",
                 "Abdômen: Abdominais 4x30",
                 "Ombros: Elevação lateral 4x12 ",
-                "Cardio: Corrida 30min"
+                "Cardio: Esteira ou Bike 30min"
             ],
             3: [
                 "Dia 1: Peito e Tríceps",
@@ -148,12 +148,12 @@ function gerarRotinaTreino(params) {
                 "Dia 3: Pernas, Abdômen e Cardio",
                 "Pernas: Agachamento 5x12, Leg press 5x12",
                 "Abdômen: Abdominais 4x30",
-                "Cardio: Corrida 30min"
+                "Cardio: Esteira ou Bike 30min"
             ],
             5: [
                 "Dia 1: Peito e Cardio",
                 "Peito: Supino reto 5x8, Flexões 5x8",
-                "Cardio: Corrida 30min",
+                "Cardio: Esteira ou Bike 30min",
                 "Dia 2: Costas",
                 "Costas: Remada curvada 4x12, Puxada na polia 5x8",
                 "Dia 3: Pernas",
